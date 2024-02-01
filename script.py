b719d83750d0f376eca09fbaa67f22489aae5284
Tholkappiar <tholkappiar2003@gmail.com>
2024-02-01 12:45:44 +0530

    fix: cancelling the script stops the port.

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import random
+import os
 from flask import Flask, render_template, request, jsonify
 import pyautogui
 import time
@@ -22,7 +23,6 @@ def type_like_human(text):
         time.sleep(0.05 + 0.1 * random.random())
         # time.sleep(0.1 + 0.2 * random.random())
 
-
     # Select & Delete selected text - TODO: this may cause issue in online exams 
     pyautogui.keyDown('ctrl')
     pyautogui.keyDown('shift')
@@ -32,5 +32,44 @@ def type_like_human(text):
 
     pyautogui.press('backspace')
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # ANSI escape codes for professional colors
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    RESET = '\033[0m'
+
+     # Banner
+    banner = """
+    ████████╗██╗   ██╗██████╗  ██████╗ ██████╗  ██████╗ ████████╗
+    ╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔═══██╗██╔══██╗██╔═══██╗╚══██╔══╝
+       ██║    ╚████╔╝ ██████╔╝██║   ██║██████╔╝██║   ██║   ██║   
+       ██║     ╚██╔╝  ██╔═══╝ ██║   ██║██╔══██╗██║   ██║   ██║   
+       ██║      ██║   ██║     ╚██████╔╝██████╔╝╚██████╔╝   ██║   
+       ╚═╝      ╚═╝   ╚═╝      ╚═════╝ ╚═════╝  ╚═════╝    ╚═╝   
+                                                                 """
+
+    # Print banner
+    print(f"{YELLOW}{banner}{RESET}")
+
+    # Repo link
+    repo_link = "https://github.com/Tholkappiar"
+
+    # Prompt the user to check out a repo and give it a star
+    print(f"{YELLOW}If interested, take a look at our repository and give it a star: {GREEN}{repo_link}{RESET}")
+    
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Shutting down the server...")
+        shutdown_server()
